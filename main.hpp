f6da9257e15c1951cafe02c2d8b7f2c38c62ef57
Sricharan Vagvala <charansv19@gmail.com>
2026-05-11 15:22:35 -0700

    completed the program

--- main.hpp
+++ main.hpp
@@ -11,7 +11,36 @@ int finduserstring(char[], int, char[], int);
 
 int finduserstring(char cstr[], int cstrlen, char userstr[], int userlen)
 {
-    // TODO: scan cstr for the first occurrence of userstr, return its starting
+    if (userlen == 0)
+    {
+        return 0;
+    }
+
+    if (cstrlen <= 0 || userlen < 0 || userlen > cstrlen)
+    {
+        return -1;
+    }
+
+    for (int i = 0; i <= cstrlen - userlen; i++)
+    {
+        bool match = true;
+
+        for (int j = 0; j < userlen; j++)
+        {
+            if (cstr[i + j] != userstr[j])
+            {
+                match = false;
+                break;
+            }
+        }
+
+        if (match)
+        {
+            return i;
+        }
+    }
+
+    return -1;
 }
 
 #endif
